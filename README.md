951326aa76b311a4237fe4579d50fcb4420b91e5
Santhosh Kumar Prakash <santhoshpkumar@users.noreply.github.com>
2018-08-25 20:01:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,151 @@
-# 3D-Motion-Planning
-3D Motion Planning
+## Project: 3D Motion Planning
+![Quad Image](./misc/enroute.png)
+
+---
+
+#### Explain the Starter Code
+
+#### 1. Functionality provided in `motion_planning.py` and `planning_utils.py`
+
+***'motion_planning'*** script controls the basic actions the drone can take.
+
+There are seven states that drone can be in.
+![Quad States](./misc/states.png)
+
+****MANUAL**** : This is the default state where the script got no contorl of the drone
+
+****ARMING**** : The scripts takes control over the drone, once armed the drone takes steering, navigation input from the script.
+
+****PLANNING**** : The navigation path is derived using the A* algorithm for the given map which is used as input for the waypoint to guide the drone to follow the waypoint. Additional the initial altiite, source and destination are all set in this planning phase. This forms the * PLAN * for the drone.
+
+****TAKEOFF**** : The drones takes off to the intial set altitude vertically. It uses the callback to determined the velocity and position of the drone to hover to the set takeoff altitude.
+
+****WAYPOINT**** : This is the heart of the drone motion, which determines the current drone spot and gudies it through the planned waypoint unitl the destinaiton point is reached.
+
+****LANDING**** : Once the destiantion point as per the plan is reached. The drone is instructed to land. Only veritical motion and no horizontal movement in this phase.
+
+****DISARMING**** : Drone is disconnected from the auto pilot. Teh script can no longer contorl the drone.
+
+***'planning_utils'*** contain a basic planning implementation that includes reading the 2.5d plan and generating the 2d grid which can be used to navigate the drone in. It also holds the A* implementation that generates the path to be used to reach the destination.
+
+### Implementing Your Path Planning Algorithm
+
+#### 1. Set your global home position
+
+Read the first line of the csv file, extract lat0 and lon0.
+
+![Quad States](./misc/get_home_position.png)
+
+Extract lat0,  long0 as floating point values and use the self.set_home_position() method to set global home.
+
+And here is a lovely picture of our downtown San Francisco environment from above!
+
+![Map of SF](./misc/map.png)
+
+Simulated city environment
+
+![Map of SF](./misc/city_map.png)
+
+Pixelated view of the map (altitue 0)
+
+![Map of SF](./misc/dig_city_map.png)
+
+#### 2. Set your current local position
+
+![Quad States](./misc/set_home_position.png)
+
+We will set the global home and global postion to the retrived (lan, lon) value. We will now convert this to local co-ordinates.
+
+#### 3. Set grid start position from local position
+
+![Map of SF](./misc/set_alt_and_safe_dist.png)
+We now generate 2D view of the map (altitude 5 m and safe distance of 5 m from obstacles), the start local position calculated above is marked by an 'X'
+
+![Map of SF](./misc/dig_city_map_5.png)
+
+#### 4. Set grid goal position from geodetic coords
+This step is to add flexibility to the desired goal location. Should be able to choose any (lat, lon) within the map and have it rendered to a goal location on the grid.
+
+![set goal position of SF](./misc/set_goal_position_1.png)
+The calculated goal position will be fed into motion planning. The goal position is marked in orange 'x' on the map
+
+![Map of goal position](./misc/map_goal_position_1.png)
+
+Let's calculate the goal postioin for another random position on the map.
+
+![set goal position of SF](./misc/set_goal_position_2.png)
+![Map of goal position](./misc/map_goal_position_2.png)
+
+#### 5. Modify A* to include diagonal motion (or replace A* altogether)
+
+Let's first  run the vanial A* with only horizontal and veritcal motion allowed. A* algorithm is found in planning_utils()
+
+![A Star Search](./misc/a_star_search.png)
+Even thought it seems that path got diagonal lines, but when in details it is nothing but smail steps motion. The safe distance has been set to 0, for better visualisation of the path taken.
+
+![A Star Search Path](./misc/a_star_search_path.png)
+
+Now we will update the A* implementation to include diagonal motions on the grid that have a cost of sqrt(2).
+
+    WEST = (0, -1, 1)
+    EAST = (0, 1, 1)
+    NORTH = (-1, 0, 1)
+    SOUTH = (1, 0, 1)
+
+    # New addition for diagonal path
+    SOUTH_EAST = (1, 1, sqrt(2))
+    NORTH_EAST = (-1, 1, sqrt(2))
+    SOUTH_WEST = (1, -1, sqrt(2))
+    NORTH_WEST = (-1, -1, sqrt(2))
+
+![A Star Modified Search](./misc/a_star_search_mod.png)
+
+![A Star Modified Search Path](./misc/a_star_search_path_mod.png)
+
+Video of drone completing the unpruned path with multiple waypoint one for each grid.
+
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/MUZstiaI4oE/0.jpg)](https://www.youtube.com/watch?v=MUZstiaI4oE)
+
+#### 6. Cull waypoints 
+
+The idea is simply to prune the path of unnecessary waypoints using collinearity test.
+
+Prune path points from `path` using collinearity.
+
+
+    def point(p):
+        return np.array([p[0], p[1], 1.]).reshape(1, -1)
+
+    def collinearity_check(p1, p2, p3):
+        m = np.concatenate((p1, p2, p3), 0)
+        det = np.linalg.det(m)
+        return abs(det) < epsilon
+
+    pruned_path = [p for p in path]
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = point(pruned_path[i])
+        p2 = point(pruned_path[i+1])
+        p3 = point(pruned_path[i+2])
+
+        # If the 3 points are in a line remove
+        # the 2nd point.
+        # The 3rd point now becomes and 2nd point
+        # and the check is redone with a new third point
+        # on the next iteration.
+        if collinearity_check(p1, p2, p3):
+            # Something subtle here but we can mutate
+            # `pruned_path` freely because the length
+            # of the list is check on every iteration.
+            pruned_path.remove(pruned_path[i+1])
+        else:
+            i += 1
+
+
+[![A Star Pruned Goal One](https://img.youtube.com/vi/2ksn3gCaLuM/0.jpg)](https://www.youtube.com/watch?v=2ksn3gCaLuM)
+
+[![A Star Pruned Goal Two](https://img.youtube.com/vi/X1YSSbRBcYA/0.jpg)](https://www.youtube.com/watch?v=X1YSSbRBcYA)
+
+### Execute the flight
+#### 1. Does it work?
+It works!
---
+++ motion_planning.py
@@ -0,0 +1,204 @@
+import argparse
+import time
+import msgpack
+from enum import Enum, auto
+
+import numpy as np
+
+from planning_utils import a_star, heuristic, create_grid, get_home_position, collinearity_prune
+from udacidrone import Drone
+from udacidrone.connection import MavlinkConnection
+from udacidrone.messaging import MsgID
+from udacidrone.frame_utils import global_to_local
+
+
+class States(Enum):
+    MANUAL = auto()
+    ARMING = auto()
+    TAKEOFF = auto()
+    WAYPOINT = auto()
+    LANDING = auto()
+    DISARMING = auto()
+    PLANNING = auto()
+
+
+class MotionPlanning(Drone):
+
+    def __init__(self, connection, global_goal_position=None):
+        super().__init__(connection)
+
+        self.target_position = np.array([0.0, 0.0, 0.0])
+        self.waypoints = []
+        self.in_mission = True
+        self.check_state = {}
+        self.global_goal_position = global_goal_position
+
+        # initial state
+        self.flight_state = States.MANUAL
+
+        # register all your callbacks here
+        self.register_callback(MsgID.LOCAL_POSITION, self.local_position_callback)
+        self.register_callback(MsgID.LOCAL_VELOCITY, self.velocity_callback)
+        self.register_callback(MsgID.STATE, self.state_callback)
+
+    def local_position_callback(self):
+        if self.flight_state == States.TAKEOFF:
+            if -1.0 * self.local_position[2] > 0.95 * self.target_position[2]:
+                self.waypoint_transition()
+        elif self.flight_state == States.WAYPOINT:
+            if np.linalg.norm(self.target_position[0:2] - self.local_position[0:2]) < 1.0:
+                if len(self.waypoints) > 0:
+                    self.waypoint_transition()
+                else:
+                    if np.linalg.norm(self.local_velocity[0:2]) < 1.0:
+                        self.landing_transition()
+
+    def velocity_callback(self):
+        if self.flight_state == States.LANDING:
+            if self.global_position[2] - self.global_home[2] < 0.1:
+                if abs(self.local_position[2]) < 0.01:
+                    self.disarming_transition()
+
+    def state_callback(self):
+        if self.in_mission:
+            if self.flight_state == States.MANUAL:
+                self.arming_transition()
+            elif self.flight_state == States.ARMING:
+                if self.armed:
+                    self.plan_path()
+            elif self.flight_state == States.PLANNING:
+                self.takeoff_transition()
+            elif self.flight_state == States.DISARMING:
+                if ~self.armed & ~self.guided:
+                    self.manual_transition()
+
+    def arming_transition(self):
+        self.flight_state = States.ARMING
+        print("arming transition")
+        self.arm()
+        self.take_control()
+
+    def takeoff_transition(self):
+        self.flight_state = States.TAKEOFF
+        print("takeoff transition")
+        self.takeoff(self.target_position[2])
+
+    def waypoint_transition(self):
+        self.flight_state = States.WAYPOINT
+        print("waypoint transition")
+        self.target_position = self.waypoints.pop(0)
+        print('target position', self.target_position)
+        self.cmd_position(self.target_position[0], self.target_position[1], self.target_position[2], self.target_position[3])
+
+    def landing_transition(self):
+        self.flight_state = States.LANDING
+        print("landing transition")
+        self.land()
+
+    def disarming_transition(self):
+        self.flight_state = States.DISARMING
+        print("disarm transition")
+        self.disarm()
+        self.release_control()
+
+    def manual_transition(self):
+        self.flight_state = States.MANUAL
+        print("manual transition")
+        self.stop()
+        self.in_mission = False
+
+    def send_waypoints(self):
+        print("Sending waypoints to simulator ...")
+        data = msgpack.dumps(self.waypoints)
+        self.connection._master.write(data)
+
+    def plan_path(self):
+        self.flight_state = States.PLANNING
+        print("Searching for a path ...")
+        TARGET_ALTITUDE = 5
+        SAFETY_DISTANCE = 5
+
+        self.target_position[2] = TARGET_ALTITUDE
+        filename = 'colliders.csv'
+
+        # TODO: read lat0, lon0 from colliders into floating point values
+        lat0, lon0 = get_home_position(filename)
+
+        # TODO: set home position to (lon0, lat0, 0)
+        self.set_home_position(lon0, lat0, 0)
+
+        # TODO: retrieve current global position
+        #global_position = [self._longitude, self._latitude, self._altitude]
+
+        # TODO: convert to current local position using global_to_local()
+        local_north, local_east, _ = global_to_local(self.global_position, self.global_home)
+
+        print('global home {0}, global position {1}, local position {2}'.format(self.global_home, self.global_position,
+                                                                         self.local_position))
+        # Read in obstacle map
+        data = np.loadtxt(filename , delimiter=',', dtype='Float64', skiprows=2)
+        
+        # Define a grid for a particular altitude and safety margin around obstacles
+        grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
+        print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
+        # Define starting point on the grid (this is just grid center)
+        grid_start = (-north_offset, -east_offset)
+        # TODO: convert start position to current position rather than map center
+        grid_start_north = int(np.ceil(local_north - north_offset))
+        grid_start_east = int(np.ceil(local_east - east_offset))
+        grid_start = (grid_start_north, grid_start_east)
+
+        # Set goal as some arbitrary position on the grid
+        if False:
+            grid_goal = (-north_offset + 10, -east_offset + 10)
+        else:
+        # TODO: adapt to set goal as latitude / longitude position and convert
+            local_goal_north, local_goal_east, _ = global_to_local(self.global_goal_position, self.global_home)
+            grid_goal_north = int(np.ceil(local_goal_north - north_offset))
+            grid_goal_east = int(np.ceil(local_goal_east - east_offset))
+            grid_goal = (grid_goal_north, grid_goal_east)
+
+        # Run A* to find a path from start to goal
+        # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
+        # or move to a different search space such as a graph (not done here)
+        print('Local Start and Goal: ', grid_start, grid_goal)
+        path, _ = a_star(grid, heuristic, grid_start, grid_goal)
+        # TODO: prune path to minimize number of waypoints
+        # TODO (if you're feeling ambitious): Try a different approach altogether!
+        path = collinearity_prune(path)
+        # Convert path to waypoints
+        waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
+        # Set self.waypoints
+        self.waypoints = waypoints
+        # TODO: send waypoints to sim (this is just for visualization of waypoints)
+        self.send_waypoints()
+
+    def start(self):
+        self.start_log("Logs", "NavLog.txt")
+
+        print("starting connection")
+        self.connection.start()
+
+        # Only required if they do threaded
+        # while self.in_mission:
+        #    pass
+
+        self.stop_log()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=5760, help='Port number')
+    parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
+    parser.add_argument('--goal_lat', type=float, default=37.796342, help="Goal latitude")
+    parser.add_argument('--goal_lon', type=float, default=-122.398248, help="Goal longitude")
+    parser.add_argument('--goal_alt', type=float, default=5, help="Goal altitude")
+    args = parser.parse_args()
+
+    conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), timeout=60)
+    global_goal_position = np.array([args.goal_lon, args.goal_lat, args.goal_alt], dtype='Float64') #np.fromstring(f'{args.goal_lon},{args.goal_lat},{args.goal_alt}', dtype='Float64', sep=',')
+  
+    drone = MotionPlanning(conn, global_goal_position)
+    time.sleep(1)
+
+    drone.start()
